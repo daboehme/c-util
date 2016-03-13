791ee14946080dd14a143308788900c823d5b02c
David Boehme <dboehme42@gmail.com>
2016-03-13 13:28:23 -0700

    Add basic documentation for vlenc

--- src/vlenc.h
+++ src/vlenc.h
@@ -11,6 +11,12 @@
 extern "C" {
 #endif
 
+/*! \brief Write 64-bit value into buffer using variable-length encoding.
+ *  
+ * \param  val  64-bit unsigned integer value to be written.
+ * \param  buf  Character buffer. Must be large enough to hold 10 bytes.
+ * \return      Number of bytes written.
+ */
 inline size_t
 vlenc_u64(uint64_t val, unsigned char* buf)
 {
@@ -27,6 +33,12 @@ vlenc_u64(uint64_t val, unsigned char* buf)
     return nbytes;
 }
 
+/*! \brief Read variable-length encoded 64-bit value from buffer.
+ *
+ * \param  buf  Buffer to read from.
+ * \param  inc  The function increments this value by the number of bytes read.
+ * \return      The decoded value.
+ */    
 inline uint64_t
 vldec_u64(const unsigned char* buf, size_t* inc)
 {
